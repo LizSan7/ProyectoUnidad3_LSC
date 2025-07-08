d469f237f88d5f612e13c1708e4d050ce4d745bd
LizSan7 <20223tn031@utez.edu.mx>
2025-07-08 16:36:07 -0600

    Subiendo JWT con Swagger

--- Unidad3/src/main/java/utez/edu/mx/unidad3/modules/auth/AuthController.java
+++ Unidad3/src/main/java/utez/edu/mx/unidad3/modules/auth/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import utez.edu.mx.unidad3.modules.auth.dto.LoginRequestDTO;
+import utez.edu.mx.unidad3.modules.user.BeanUser;
 import utez.edu.mx.unidad3.utils.APIResponse;
 
 @RestController
@@ -19,5 +20,11 @@ public class AuthController {
     public ResponseEntity<APIResponse> doLogin(@RequestBody LoginRequestDTO payload){
         APIResponse response = authService.doLogin(payload);
         return new ResponseEntity<>(response,response.getStatus());
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<APIResponse> doRegister(@RequestBody BeanUser payload){
+        APIResponse response = authService.register(payload);
+        return new ResponseEntity<>(response, response.getStatus());
     }
 }
--- Unidad3/src/main/java/utez/edu/mx/unidad3/modules/auth/AuthService.java
+++ Unidad3/src/main/java/utez/edu/mx/unidad3/modules/auth/AuthService.java
@@ -11,6 +11,9 @@ import utez.edu.mx.unidad3.modules.user.UserRepository;
 import utez.edu.mx.unidad3.security.jwt.JWTUtils;
 import utez.edu.mx.unidad3.security.jwt.UDService;
 import utez.edu.mx.unidad3.utils.APIResponse;
+import utez.edu.mx.unidad3.utils.PasswordEncoder;
+
+import java.sql.SQLException;
 
 @Service
 public class AuthService {
@@ -25,19 +28,41 @@ public class AuthService {
 
     @Transactional(readOnly = true)
     public APIResponse doLogin(LoginRequestDTO payload){
-        BeanUser found = userRepository. findByUsernameAndPassword(
-                payload.getUsername(),
-                payload.getPassword()
-        ).orElse(null);
+
 
         try{
+            BeanUser found = userRepository. findByUsername(payload.getUsername()).orElse(null);
             if(found == null) return new APIResponse("Usuario no econtrado", true, HttpStatus.NOT_FOUND);
+
+            if(PasswordEncoder.verifyPassword(payload.getPassword(), found.getPassword()))
+                return new APIResponse("Las contraseñas no coinciden", true, HttpStatus.BAD_REQUEST);
+
             UserDetails ud = udService.loadUserByUsername(found.getUsername());
             String token = jwtUtils.genereteToken(ud);
             return new APIResponse("Operación exitosa", token,false, HttpStatus.OK);
         }catch(Exception e){
             e.printStackTrace();
             return new APIResponse("Error al iniciar sesión", true, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Transactional(rollbackFor = {SQLException.class,Exception.class})
+    public APIResponse register(BeanUser payload){
+        try {
+            BeanUser found = userRepository.findByUsername(payload.getUsername()).orElse(null);
+            if (found != null) return new APIResponse("Usuario ya existente",true,HttpStatus.BAD_REQUEST); {
+                payload.setPassword(PasswordEncoder.encodePassword(payload.getPassword()));
+                userRepository.save(payload);
+
+                return new APIResponse("Operacion exitosa",true,HttpStatus.CREATED);
+            }
+        }catch (Exception ex) {
+            ex.printStackTrace();
+            return new APIResponse(
+                    "Error al iniciar sesion",
+                    true,
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
         }
     }
 }
